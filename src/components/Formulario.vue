423a43201c0f3cc2dce1837b07775ed8e14d2b32
joaoricardoM <joao_pia_mds@hotmail.com>
2023-02-14 16:48:46 -0300

    update code

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -12,53 +12,19 @@
           placeholder="Qual tarefa você deseja iniciar?"
         />
       </div>
-      <div class="column">
-        <div
-          class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-          <section>
-            <strong>{{ tempoEmSegundos }}</strong>
-          </section>
-          <button class="button" @click="iniciar">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="Finalizar">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
-      </div>
+      <div class="column"><Temporizador /></div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Temporizador from "./Temporizador.vue";
 
 export default defineComponent({
   name: "FormularioItem",
-  data() {
-    return {
-      tempoEmSegundos: 0,
-    };
-  },
-  methods: {
-    iniciar() {
-      // inicia a contagem
-      setInterval(() => {
-        console.log("incrementando o contador");
-        this.tempoEmSegundos += 1;
-      }, 1000);
-    },
-    Finalizar() {
-      // termina a contagem
-      console.log("finalizando");
-    },
+  components: {
+    Temporizador,
   },
 });
 </script>
