1a10b0782a56796766e296bf39a1f0f129d28b7e
joaoricardoM <joao_pia_mds@hotmail.com>
2023-02-15 12:29:54 -0300

    create tarefa

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <div class="columns">
+      <div class="column is -7">Descrição da tarefa</div>
+      <div class="column">
+        <Cronometro tempoEmSegundos="15" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="ts">
+import { defineComponent } from 'vue';
+import Cronometro from "./Cronometro.vue";
+
+export default defineComponent ({
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Tarefa',
+    components: {
+        Cronometro
+    }
+    })
+</script>
+
+<style scoped>
+.box {
+  background: #faf0ca;
+}
+</style>
