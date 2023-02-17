b24520e70e7a793ca9142777f6adcc94983b1c2d
joaoricardoM <joao_pia_mds@hotmail.com>
2023-02-17 10:25:29 -0300

    create component box

---
+++ src/components/Box.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+// eslint-disable-next-line vue/multi-word-component-names
+export default defineComponent({ name: "Box" });
+</script>
+
+<style scoped>
+.box {
+  background: #faf0ca;
+}
+</style>
