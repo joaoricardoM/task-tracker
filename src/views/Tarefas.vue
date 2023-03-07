0864b4fb11cbcc789399723a4d7c3d363913ee78
joaoricardoM <joao_pia_mds@hotmail.com>
2023-03-07 10:31:25 -0300

    create tarefas

---
+++ src/views/Tarefas.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="column is-three-quarter conteudo">
+    <FormularioItem @aoSalvarTarefa="salvarTarefa" />
+    <div class="lista">
+      <Tarefa
+        v-for="(tarefa, index) in tarefas"
+        :key="index"
+        :tarefa="tarefa"
+      />
+      <Box> Você não está muito produtivo hoje ;( </Box>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import FormularioItem from "../components/Formulario.vue";
+import Tarefa from "./components/Tarefa.vue";
+import ITarefa from "../interface/Itarefa";
+import Box from "./components/Box.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    FormularioItem,
+    Tarefa,
+    Box,
+  },
+  data() {
+    return {
+      tarefas: [] as ITarefa[],
+      modoEscuroAtivo: false,
+    };
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa);
+    },
+    trocarTema(modoEscuroAtivo: boolean) {
+      this.modoEscuroAtivo = modoEscuroAtivo;
+    },
+  },
+});
+</script>
+
+<style>
+.lista {
+  padding: 1.25rem;
+}
+main {
+  --bg-primary: #fff;
+  --text-primary: #000;
+}
+
+main.modo-escuro {
+  --bg-primary: #2b2d42;
+  --text-primary: #ddd;
+}
+
+.conteudo {
+  background-color: var(--bg-primary);
+}
+</style>
