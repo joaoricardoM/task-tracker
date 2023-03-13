8155180cacdc6f73578758e89258cc836ad0f818
joaoricardoM <joao_pia_mds@hotmail.com>
2023-03-13 15:31:40 -0300

    create new forms

---
+++ src/views/Projetos/Formulario.vue
@@ -0,0 +1,85 @@
+<template>
+  <section class="projetos">
+    <h1 class="title">Formulario</h1>
+
+    <form @submit.prevent="salvar">
+      <div class="field">
+        <label for="nomeDoProjeto" class="label"> Nome do Projeto </label>
+        <input
+          type="text"
+          class="input"
+          v-model="nomeDoProjeto"
+          id="nomeDoProjet"
+        />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script lang="ts">
+import { useStore } from "@/store";
+import { defineComponent } from "vue";
+// import Projetos from "../Projetos.vue";
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Formulario",
+  props: {
+    id: {
+      type: String,
+    },
+  },
+  mounted() {
+    if (this.id) {
+      const projeto = this.store.state.projetos.find(
+        (proj) => proj.id == this.id
+      );
+      this.nomeDoProjeto = projeto?.nome || "";
+    }
+  },
+  data() {
+    return {
+      nomeDoProjeto: "",
+    };
+  },
+  methods: {
+    salvar() {
+      if (this.id) {
+        this.store.commit("ALTERA_PROJETO", {
+          id: this.id,
+          nome: this.nomeDoProjeto,
+        });
+      } else {
+        this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
+        //
+      }
+      this.nomeDoProjeto = "";
+      this.$router.push("/projetos");
+    },
+  },
+  setup() {
+    const store = useStore();
+    return {
+      store,
+    };
+  },
+});
+</script>
+
+<style scoped>
+main {
+  --bg-primary: #fff;
+  --text-primary: #000;
+}
+
+main.modo-escuro {
+  --bg-primary: #2b2d42;
+  --text-primary: #ddd;
+}
+.projetos {
+  padding: 1.25rem;
+}
+</style>
