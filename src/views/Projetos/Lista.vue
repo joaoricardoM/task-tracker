4cf4eed8848b4206ca47382d8e29c4cb515ce286
joaoricardoM <joao_pia_mds@hotmail.com>
2023-04-10 08:53:54 -0300

    feat: change routes

---
+++ src/views/Projetos/Lista.vue
@@ -0,0 +1,60 @@
+<template>
+  <section>
+    <router-link to="/projetos/novo" class="button">
+      <span class="icon is-small">
+        <i class="fas fa-plus"></i>
+      </span>
+      <span>Novo projeto</span>
+    </router-link>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="projeto in projetos" :key="projeto.id">
+          <td>{{ projeto.id }}</td>
+          <td>{{ projeto.nome }}</td>
+          <td>
+            <router-link :to="`/projetos/${projeto.id}`" class="button">
+              <span class="icon is-small">
+                <i class="fas fa-pencil-alt"></i>
+              </span>
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
+
+<script lang="ts">
+import { useStore } from "@/store";
+import { computed, defineComponent } from "vue";
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Lista",
+  setup() {
+    const store = useStore();
+    return {
+      projetos: computed(() => store.state.projetos),
+    };
+  },
+});
+</script>
+
+<style scoped>
+main {
+  --bg-primary: #fff;
+  --text-primary: #000;
+}
+
+main.modo-escuro {
+  --bg-primary: #2b2d42;
+  --text-primary: #ddd;
+}
+</style>
