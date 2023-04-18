8ed152910a232b23dbab188e5fc76dc8c863ccd3
joaoricardoM <joao_pia_mds@hotmail.com>
2023-04-18 10:59:00 -0300

    feat: add notificações

---
+++ src/components/Notificacoes.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="notificacoes">
+    <article class="message is-success">
+      <div class="message-header">Atencao</div>
+      <div class="message-body">Aqui vai um teste de notificação</div>
+    </article>
+  </div>
+  <h1>Notificação</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Notificacoes",
+});
+</script>
